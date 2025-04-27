7994905e43f3698314be24b81571100ae15b76a1
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 22:47:36 +0100

    feat:ad context search data of api

---
+++ src/context/searchContext.jsx
@@ -0,0 +1,39 @@
+"use client";
+import { createContext, useContext, useState } from "react";
+
+const SearchContext = createContext();
+
+export function SearchProvider({ children }) {
+  const [results, setResults] = useState([]);
+
+  const searchPlayers = async (query) => {
+    try {
+      const response = await fetch("http://localhost:3001/search", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ q: query }),
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setResults(data); // Atualiza os resultados no contexto
+      } else {
+        console.error("Erro ao buscar jogadores");
+      }
+    } catch (error) {
+      console.error("Erro na requisição:", error);
+    }
+  };
+
+  return (
+    <SearchContext.Provider value={{ results, searchPlayers }}>
+      {children}
+    </SearchContext.Provider>
+  );
+}
+
+export function useSearch() {
+  return useContext(SearchContext);
+}
