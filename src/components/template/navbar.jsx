b9388e3c7b150bec21590f47190af0b9b11ab979
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 18:03:33 +0100

    feat:add component navbar with links for navigation

---
+++ src/components/template/navbar.jsx
@@ -0,0 +1,19 @@
+import Link from "next/link";
+
+export function Navbar() {
+  return (
+    <>
+      <nav className="flex items-center justify-between bg-white text-black h-10 py-6 max-w-[1000px] w-full mx-auto">
+        <div className="text-1xl font-bold uppercase">Basket info</div>
+        <ul className="flex space-x-8">
+          <li>
+            <Link href="/">Home</Link>
+          </li>
+          <li>
+            <Link href="/history">Histórico</Link>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
+}
