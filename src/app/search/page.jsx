3c62d95438f5dc41bf810cde6a5981a2404cb725
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 22:48:31 +0100

    feat:add context search inpage search

--- src/app/search/page.jsx
+++ src/app/search/page.jsx
@@ -1,33 +1,46 @@
+"use client";
 import { Search } from "@/components/search";
 import { Button } from "@/components/ui/button";
+import { useSearch } from "@/context/searchContext";
+import Link from "next/link";
 
 export default function SearchPage() {
+  const { results } = useSearch(); // Obtém os resultados do contexto
+
   return (
     <div className="flex flex-col items-center justify-center gap-4">
       <div className="bg-white p-4 w-full max-w-md rounded-md">
         <Search />
       </div>
       <h1 className="text-2xl font-bold text-center text-white">
-        Resultados encontrados:
+        {results.length > 0
+          ? "Resultados encontrados:"
+          : "Nenhum jogador encontrado"}
       </h1>
       <div className="flex flex-col gap-5 w-full">
-        {[1, 2].map((_, index) => (
-          <div
-            key={index}
-            className="p-2 bg-white rounded-lg shadow-lg flex justify-between items-center gap-6 w-full"
-          >
-            <div className="flex items-center gap-2">
-              <div className="w-10 h-10 rounded-full bg-black"></div>
-              <div>
-                <p className="text-xs">Nome: Juelson Júnior</p>
-                <p className="text-xs">Time: Waipos Time</p>
+        {results.length > 0 ? (
+          results.map((player, index) => (
+            <div
+              key={index}
+              className="p-2 bg-white rounded-lg shadow-lg flex justify-between items-center gap-6 w-full"
+            >
+              <div className="flex items-center gap-2">
+                <div className="w-10 h-10 rounded-full bg-black"></div>
+                <div>
+                  <p className="text-xs">Nome: {player.nome}</p>
+                  <p className="text-xs">Equipa: {player.equipe}</p>
+                </div>
               </div>
+              <Link href={`/profile?id=${player.id}`}>
+                <Button className="cursor-pointer px-6 py-3" type="button">
+                  Perfil
+                </Button>
+              </Link>
             </div>
-            <Button className="cursor-pointer px-6 py-3" type="submit">
-              Perfil
-            </Button>
-          </div>
-        ))}
+          ))
+        ) : (
+          <p className="text-white text-center">Tente pesquisar outro nome.</p>
+        )}
       </div>
     </div>
   );
