5250a624db9c23037147cda082882decfbd1e6cb
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 22:49:36 +0100

    feat:eliminate component search of home

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,9 @@
-import { Search } from "@/components/search";
 
 export default function Home() {
   return (
     <div className="flex items-center justify-center" style={{ minHeight: "calc(100vh - 5.5rem)" }}>
       <div className="flex flex-col gap-6 items-center justify-center w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-        <h1 className="text-3xl font-bold text-center ">Pesquisar jogador</h1>
-        <Search />
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ab, enim ipsa omnis, illum neque officiis placeat, commodi ex voluptatum aliquid consectetur rem quibusdam sint! Voluptates qui sit eum dignissimos!
       </div>
     </div>
   );
