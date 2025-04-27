ee5fb0c031919a92689598dd46e97a730414452a
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 18:08:48 +0100

    feat:add template main in layout base of website

---
+++ src/app/layout.jsx
@@ -0,0 +1,20 @@
+import { Main } from "@/components/template/main";
+import "./globals.css";
+
+export const metadata = {
+  title: "Basketball Stats",
+  description: "A simple app to track basketball stats",
+  icons: {
+    icon: "/favicon.png",
+  },
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="pt-br">
+      <body>
+        <Main>{children}</Main>
+      </body>
+    </html>
+  );
+}
