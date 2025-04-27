be4950ddd706939bd4c9f85879a09f7fe8ec85a0
juelsonjunior <juelsonjunior27@gmail.com>
2025-04-27 22:48:10 +0100

    feat:add context search in component search

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,16 +1,32 @@
+"use client";
+import { useState } from "react";
+import { useSearch } from "@/context/searchContext";
 import { Button } from "./ui/button";
 import { Input } from "./ui/input";
 
 export function Search() {
+  const [searchData, setSearchData] = useState("");
+  const { searchPlayers } = useSearch();
+
+  const handleSearch = async () => {
+    await searchPlayers(searchData); // Realiza a pesquisa usando o contexto // Redireciona para a página de resultados
+  };
+
   return (
     <div className="flex flex-col items-center justify-center gap-6 w-full">
       <div className="flex gap-3 w-full max-w-md">
         <Input
-          type="email"
+          type="text"
           placeholder="Pesquisar jogador"
+          value={searchData}
+          onChange={(e) => setSearchData(e.target.value)}
           className="flex-1 p-2 border-2 rounded-md"
         />
-        <Button className="cursor-pointer px-6 py-3" type="submit">
+        <Button
+          className="cursor-pointer px-6 py-3"
+          type="button"
+          onClick={handleSearch}
+        >
           Pesquisar
         </Button>
       </div>
